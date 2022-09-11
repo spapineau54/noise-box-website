e3e8686ba1f5ad43f5ce7ef4dce2f4fa70eb553a
Shawn <s.papineau92@gmail.com>
2022-09-11 00:58:35 -0400

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import Navbar from "./components/Navbar";
+import hero from "./images/destroy_hero.jpeg";
+import hitw from "./images/hitw.jpeg";
+import logo from "./images/tnbnewlogo.jpeg";
 
 function App() {
 	return (
 		<div>
-			<Navbar />
+			<Navbar logo={logo} />
 
 			<main>
 				<BrowserRouter>
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-const Navbar = () => {
+const Navbar = ({ logo }) => {
 	return (
 		<header>
-			<nav className='flex text-lg flex-row justify-between items-center py-5 mx-auto '>
-				<div></div>
+			<nav className='flex text-lg flex-row justify-between items-center p-5 mx-3'>
+				<div className='w-40'>
+					<img style={{ borderRadius: "100px" }} src={logo} />
+				</div>
 
-				<ul className='flex flex-row justify-between items-center '>
+				<ul className='hidden md:flex flex-row justify-between items-center '>
 					<li className='px-3 hover:text-2xl'>Home</li>
 					<li className='px-3 hover:text-2xl'>Venue</li>
 					<li className='px-3 hover:text-2xl'>Shows</li>
